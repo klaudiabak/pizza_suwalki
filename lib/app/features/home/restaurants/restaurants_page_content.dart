068c796c922a95213bd1442a69de51437f4351fc
Klaudia Bąk <aoimesan@gmail.com>
2023-02-26 18:11:55 +0100

    move home and login folder into features folder

--- lib/app/features/home/restaurants/restaurants_page_content.dart
+++ lib/app/features/home/restaurants/restaurants_page_content.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:pizza_suwalki/app/home/restaurants/cubit/restaurants_cubit.dart';
+import 'package:pizza_suwalki/app/features/home/restaurants/cubit/restaurants_cubit.dart';
 
 class RestaurantsPageContent extends StatelessWidget {
   const RestaurantsPageContent({
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:pizza_suwalki/app/home/home_page.dart';
-import 'package:pizza_suwalki/app/login/login_page.dart';
+import 'package:pizza_suwalki/app/features/home/home_page.dart';
+import 'package:pizza_suwalki/app/features/login/login_page.dart';
 import 'firebase_options.dart';
 
 void main() async {
